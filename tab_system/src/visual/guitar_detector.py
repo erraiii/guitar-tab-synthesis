835bcc76c585319feed4a74a59f788cce161e41a
ravimhingele <aivanova261203@gmail.com>
2026-04-08 12:41:12 +0300

    Add guitar tracking

--- tab_system/src/visual/guitar_detector.py
+++ tab_system/src/visual/guitar_detector.py
@@ -3,15 +3,16 @@ from utils.parsing import parse_guitar_detections
 
 
 class GuitarDetector:
-    def __init__(self, model_path: str, conf: float = 0.15):
+    def __init__(self, model_path: str, conf: float = 0.2):
         self.model = YOLO(model_path)
         self.conf = conf
         self.class_names = self.model.names
 
     def detect(self, image, time=None):
-        results = self.model.predict(
+        results = self.model.track(
             image,
             conf=self.conf,
+            persist=True,
             verbose=False
         )
 
@@ -26,35 +27,3 @@ class GuitarDetector:
             time
         )
 
-
-class GuitarDetectionPipeline:
-    def __init__(self, visual_processor, detector):
-        self.vp = visual_processor
-        self.detector = detector
-
-    def process_notes(self, notes):
-        """
-        Для каждой ноты достаёт кадр и делает детекцию грифа
-        """
-        results = []
-        prev_fret = None
-
-        for note in notes:
-            t = note.start
-
-            frame = self.vp.get_frame_at(t)
-
-            if frame is None:
-                results.append(prev_fret)
-                continue
-
-            fret = self.detector.detect(frame, time=t)
-
-            # fallback если YOLO не сработал
-            if fret is None or len(fret.frets) == 0:
-                fret = prev_fret
-
-            prev_fret = fret
-            results.append(fret)
-
-        return results
